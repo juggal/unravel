2ceda9fe0127a5971a6845d5d56b1fc8ba5db31e
juggal <cipherxo1@pm.me>
2019-08-15 01:26:00 +0530

    vue-carousel used

---
+++ src/components/Cards.vue
@@ -0,0 +1,46 @@
+<template lang="html">
+  <div>
+    <carousel
+    autoplay
+    perPage="1"
+    easing
+    >
+      <slide
+        v-for="(text, i) in texts"
+        v-bind:key="text"
+        >
+        <b-card
+          tag="article"
+          style="max-width: 20rem;"
+          class="mb-2 text-center"
+           bg-variant="dark"
+           text-variant="white"
+           v-bind:header="`Card  ` + parseInt(i + 1)"
+        >
+          <b-card-text>{{text}}</b-card-text>
+        </b-card>
+      </slide>
+    </carousel>
+  </div>
+</template>
+
+<script>
+import { Carousel, Slide } from 'vue-carousel';
+export default {
+  components: {
+    Carousel,
+    Slide
+  },
+  data() {
+    return {
+      texts: [
+        `Some quick example 1`,
+        `Some quick example 2`
+      ]
+    }
+  }
+}
+</script>
+
+<style lang="css" scoped>
+</style>
--- src/views/Round1.vue
+++ src/views/Round1.vue
@@ -1,13 +1,35 @@
 <template lang="html">
-  <div class="">
-    Hello
+  <div>
+    <b-card no-body>
+      <b-tabs
+      active-nav-item-class="btn-info"
+      pills card
+      >
+        <b-tab v-bind:title-link-class="'tab-color'" title="Clues" active>
+          <b-card-text><Cards /></b-card-text>
+        </b-tab>
+        <b-tab v-bind:title-link-class="'tab-color'" title="Questions">
+          <b-card-text>Tab Contents 2</b-card-text>
+        </b-tab>
+        <b-tab v-bind:title-link-class="'tab-color'" title="Final" disabled>
+          <b-card-text>Tab Contents 2</b-card-text>
+        </b-tab>
+      </b-tabs>
+    </b-card>
   </div>
 </template>
 
 <script>
+import Cards from '../components/Cards'
 export default {
+  components: {
+    Cards
+  }
 }
 </script>
 
-<style lang="css" scoped>
+<style lang="css">
+  .tab-color{
+    color: black;
+  }
 </style>
