3e62a467d518de58edfe884514eca43ca5035ca8
juggal <cipherxo1@pm.me>
2019-08-18 23:44:51 +0530

    iterating through question(buggy)

--- src/components/Questions.vue
+++ src/components/Questions.vue
@@ -1,11 +1,21 @@
 <template lang="html">
-  <b-container>
+  <b-container
+  class="container"
+  >
     <b-card
-    v-for="(que, i) in questions[round - 1].active"
+    v-for="(que, i) in questions[round - 1]"
     v-bind:key="que"
-
+    v-bind:title="i + 1"
     >
-    <b-card-text>{{que}}</b-card-text>
+      <b-card-text>{{que.active[0].question}}</b-card-text>
+      <b-form-group>
+        <b-form-radio
+        v-for="(option, i) in questions[round - 1].active[0].options"
+        v-bind:key="option"
+        v-model="selected">
+        {{option}}</b-form-radio>
+        <b-button variant="outline-dark">Check</b-button>
+      </b-form-group>
     </b-card>
   </b-container>
 </template>
@@ -13,6 +23,15 @@
 <script>
 import { mapState } from 'vuex';
 export default {
+  data() {
+    return {
+      selected: '',
+      options: [
+        "option1",
+        "option2"
+      ]
+    }
+  },
   props: {
     round: Number
   },
@@ -25,4 +44,7 @@ export default {
 </script>
 
 <style lang="css" scoped>
+ .container {
+   width: 35%;
+ }
 </style>
