7c378cac53eb88fd859bcf93af9f3b10ffcc8475
juggal <cipherxo1@pm.me>
2019-08-29 13:51:29 +0530

    unravel dedploy

---
+++ src/components/Story.vue
@@ -0,0 +1,25 @@
+<template lang="html">
+  <div>
+    <p
+    v-for="s in story[round - 1].unlocked"
+
+    >{{s}}</p>
+  </div>
+</template>
+
+<script>
+import { mapState, mapActions } from 'vuex'
+export default {
+  computed: {
+    ...mapState({
+      story: state => state.story
+    })
+  },
+  props: {
+    round: Number
+  }
+}
+</script>
+
+<style lang="css" scoped>
+</style>
