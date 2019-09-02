e5e5ec49a5fb3fecbc6c70c941695ee8d9223d25
juggal <37812289+juggal@users.noreply.github.com>
2019-09-02 19:51:08 +0530

    Update Login.vue

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -59,13 +59,13 @@ export default {
       },
       users: [],
       config: {
-        apiKey: "AIzaSyCY7pMmolq71sF5uSdZtCrXdSuRe4j9L2M",
-        authDomain: "suigeneris-91e90.firebaseapp.com",
-        databaseURL: "https://suigeneris-91e90.firebaseio.com",
-        projectId: "suigeneris-91e90",
-        storageBucket: "suigeneris-91e90.appspot.com",
-        messagingSenderId: "942349921706",
-        appId: "1:942349921706:web:897991dea6b3a0f3"
+        apiKey: "",
+        authDomain: "",
+        databaseURL: "",
+        projectId: "",
+        storageBucket: "",
+        messagingSenderId: "",
+        appId: ""
       },
       db: null
     }
